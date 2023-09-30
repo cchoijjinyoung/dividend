07e30ef740c7a20b310736c6407d9d1e1ee7e51b
cchoijjinyoung <cgy7127@naver.com>
2023-09-30 17:51:24 +0900

    Company, Dividend / Entitiy, Repository

---
+++ src/main/java/com/dayone/persist/entity/CompanyEntity.java
@@ -0,0 +1,25 @@
+package com.dayone.persist.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "COMPANY")
+@Getter
+@ToString
+@NoArgsConstructor
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ticker;
+    private String name;
+
+}
