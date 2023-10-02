61c8eba039ade49bb951cc8b48adeb0402078066
cchoijjinyoung <cgy7127@naver.com>
2023-10-02 18:33:21 +0900

    add : Scheduler scrap

--- src/main/java/com/dayone/Application.java
+++ src/main/java/com/dayone/Application.java
@@ -5,8 +5,10 @@ import com.dayone.scraper.Scraper;
 import com.dayone.scraper.YahooFinanceScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
