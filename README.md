fbf05330098e986f52fc7a5863e4246415e70b7e
cchoijjinyoung <cgy7127@naver.com>
2023-10-03 17:38:26 +0900

    add : Cacheable GET company dividend

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dividend
+## Redis window 설치
+- wsl --install
+- Ubuntu
+- WslRegisterDistribution failed with error: 0x80370114
+- 제어판 -> 프로그램 기능 -> 가상 머신 플랫폼 + Hyper-V 체크하기
+- 컴퓨터 재부팅 후 wsl --install 했더니 해결
+
+## Error
+###/finance/dividend/{companyName}
+- InvalidDefinitionException:model.Company = deserialize from object value
+- 
+
+## Builder 패턴
+### Company, Dividend model 클래스
+- @NoArgsConstructor 추가하면서 @Builder를 지워줬음. 어차피 멤버 변수가 많지 않기 때문에 효율적이진 않았음.
+
+## 스프링부트와 레디스는 별도의 서버이다.
--- src/main/java/com/dayone/service/FinanceService.java
+++ src/main/java/com/dayone/service/FinanceService.java
@@ -8,12 +8,14 @@ import com.dayone.persist.DividendRepository;
 import com.dayone.persist.entity.CompanyEntity;
 import com.dayone.persist.entity.DividendEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class FinanceService {
@@ -21,7 +23,9 @@ public class FinanceService {
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
 
+    @Cacheable(key = "#companyName", value = "finance") // Redis서버의 key, value와는 다름
     public ScrapedResult getDividendByCompanyName(String companyName) {
+        log.info("search Company -> " + companyName);
         // 1. 회사명을 기준으로 회사 정보를 조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다"));
@@ -32,16 +36,10 @@ public class FinanceService {
         // 3. 결과 조합 후 반환
         List<Dividend> dividends = new ArrayList<>();
         for (var entity : dividendEntities) {
-            dividends.add(Dividend.builder()
-                    .date(entity.getDate())
-                    .dividend(entity.getDividend())
-                    .build());
+            dividends.add(new Dividend(entity.getDate(), entity.getDividend()));
         }
 
-        return new ScrapedResult(Company.builder()
-                                    .ticker(company.getTicker())
-                                    .name(company.getName())
-                                    .build(),
+        return new ScrapedResult(new Company(company.getTicker(), company.getName()),
                 dividends);
     }
 }
